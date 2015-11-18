47739ded5a77a0a2e17c4a34c72744ea1fb2977d
nicodjimenez <nicodjimenez@gmail.com>
2015-11-18 14:39:42 -0500

    added instructions to run

---
+++ README.md
@@ -0,0 +1,29 @@
+# ViewImg
+
+ViewImg provides an alternative to `ssh -X` and other hacks to view a list of images on a remote server.
+To run ViewImg first clone the repo:
+```
+git clone https://github.com/nicodjimenez/ImgView.git
+```
+
+Then install the requirements:
+```
+pip install -r requirements.txt
+```
+
+To view images on port `8000` in `path/to/images/` do:
+```
+python run.py -d path/to/images/
+```
+which runs by default on port 8000.  To run on a different port, use the `-p` command line argument, for example:
+```
+python run.py -d path/to/images/ -p 8080
+```
+
+To view the images you will need to access your remote server's port.  You can either open up the port, or you can use ssh tunnelling.  If you are running ImgView on port 8000 on remote server `remote` accessible via your ssh config file as `ssh remote`, do:
+```
+ssh remote -N -L localhost:8000:localhost:8000
+```
+and point your browser to <http://0.0.0.0:8070/>.
+
+
--- run.py
+++ run.py
@@ -9,17 +9,17 @@ from flask import render_template, request
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--path', default="", type=str, help="Path of image directory.")
+parser.add_argument('-d', '--dir', default="", type=str, help="Path of image directory.")
+parser.add_argument('-p', '--port', default="8000", type=int, help="Port.")
 args = parser.parse_args()
-image_dir = args.path
-
-image_dir = os.path.abspath(image_dir)
+image_dir = os.path.abspath(args.dir)
+port = args.port
 app = Flask(__name__, 
             static_url_path = '', 
             static_folder = image_dir)
 app.debug = True
 
-@app.route("/index")
+@app.route("/")
 def index():
     types = ["*.png", "*.jpg", "*.JPG"]
     image_list = []
@@ -28,5 +28,5 @@ def index():
     image_list = [url_for("static", filename=os.path.basename(x)) for x in image_list]
     return render_template("index.html", image_list = image_list)
 
-app.run(host='0.0.0.0', port=8070, threaded=True) 
+app.run(host='0.0.0.0', port=port, threaded=True) 
 
